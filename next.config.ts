a0d1038b78ea13a799eec0217b878057246f3884
Eden Zhang <eden.zhang@sydney.edu.au>
2026-05-27 16:09:38 +1000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.0.*"],
   output: "export",
+  basePath: isProd ? "/cbir-screening-tool" : "",
   images: { unoptimized: true },
 };
 
