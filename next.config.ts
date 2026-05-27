987a1c87b8ff1b6c42e7c1fac9e1f2512130cc89
Eden Zhang <eden.zhang@sydney.edu.au>
2026-05-27 16:19:06 +1000

    try to fix logo

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,16 @@
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === "production";
+const basePath = isProd ? "/cbir-screening-tool" : "";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.0.*"],
   output: "export",
-  basePath: isProd ? "/cbir-screening-tool" : "",
+  basePath: basePath,
   images: { unoptimized: true },
+  env: {
+    NEXT_PUBLIC_BASE_PATH: basePath,
+  },
 };
 
 export default nextConfig;
